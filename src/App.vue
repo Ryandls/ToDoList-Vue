971c9931a0f5c23466a4660d2a1c60b287d7edee
Bryan Dos Santos <bryandouglasxd@gmail.com>
2021-04-13 11:45:31 -0300

    Styling Project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Body />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Body from "./components/Body";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Body,
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: rgb(53, 227, 240);
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color:rgb(233, 36, 128);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 380px;
+  height: 70vh;
+  margin: 5vh auto;
+  padding: 10px;
+  background: rgb(237, 243, 243);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  overflow: auto;
+  border-radius: 10px;
+}
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+form {
+  width: 100%;
+  display: flex;
+  border-radius: 3px;
+}
+
+form input {
+  width: 80%;
+  border: 0;
+  padding: 10px;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+
+form button {
+  width: 20%;
+  border: 0;
+  background-color: rgb(233, 36, 128);
+  color: white;
+  padding: 10px;
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
 }
 </style>
---
+++ src/components/Body.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>To Do List</h1>
+    <form>
+      <input type="text" placeholder="Entre com a uma tarefa..." />
+      <button>Enviar</button>
+    </form>
+  </div>
+  <div class="to-do-list">
+    <ol>
+      <li>
+        <span class="text">Tarefa 01</span>
+        <span class="actions">
+          <button class="complete">&#10004;</button>
+          <button class="remove">&#10006;</button>
+        </span>
+      </li>
+    </ol>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+
+</style>
